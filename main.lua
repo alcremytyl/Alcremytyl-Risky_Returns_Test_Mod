3b7ac409cf835b8e12884af945ae1d3e1b100e10
Mytyl <github.aeyip@simplelogin.com>
2024-09-06 01:19:16 -0400

    most files done

--- main.lua
+++ main.lua
@@ -2,6 +2,19 @@
 -- Alcremytyl
 
 --[[
+-- TODO 
+-- - ImGui (terror)
+-- - actually try learning how to toml
+-- - add `---@meta` and ensure description is above fields
+-- - brute force the enums and fields why lua gimme a damn table stringifier
+-- - maybe add examples via --[[ ... ]]
+--]]
+
+
+
+-- Gospel
+-- https://github.com/return-of-modding/ReturnOfModding/tree/master/docs/lua/
+--[[
 
 If someone could whip this up quick it could be cool, statsindicator reminded me that I wanted to post this, but an imgui tab where you could modify your own in game stats/add/remove items would be useful and kind of standard, I see stuff like that in a lot of games as a very early thing people start making as a mod
 ]]
@@ -11,42 +24,17 @@ current objective:
 add type hints to ReturnOfModding
 ]]
 
--- -@module 'types'
--- require("util")
+
 
 log.info("\n\n\n=====================\n\n  -- N E X T --\n\n\n")
 
 
 
-log.info(ImGui.SetClipboardText("oira"))
 
 
 
 
 
--- FIXME: figure out what's wrong w/toml
--- local toml_path = path.combine(_ENV['!config_mod_folder_path'], 'test.toml')
--- local toml_path = 'test.toml'
--- local succeeded, table = pcall(toml.decodeFromFile, toml_path)
--- log.info("toml path: "..toml_path)
--- log.info("did it work: "..tostring(succeeded))
--- log.info(table_to_string(table))
 
 
 
-_ = [[
-log.info("Successfully loaded ".._ENV["!guid"]..".")
-
-
-gui.add_to_menu_bar(function()
-   if ImGui.BeginMenu("Ayo") then
-       if ImGui.Button("Label") then
-         log.info("hi")
-       end
-       ImGui.EndMenu()
-   end
-end)
-]]
-
--- Gospel
--- https://github.com/return-of-modding/ReturnOfModding/tree/master/docs/lua/
